df1040af60097d25634028c3e9a11bd6cfb463b6
DjonniStorm <i.pazushkin@yandex.ru>
2024-05-01 02:50:40 +0400

    Новое стартовое меню 2

--- pyatnashki/src/pyatnashkiForm.java
+++ pyatnashki/src/pyatnashkiForm.java
@@ -10,13 +10,29 @@ public class pyatnashkiForm extends JFrame {
     private gamePanel gamePanel;
     private startPanel startPanel;
     public pyatnashkiForm() {
-        frame = new JFrame("Пятнашки");
+        frame = new JFrame("Пятнашки") {
+            private Image backgroundImage;
+
+            {
+                try {
+                    backgroundImage = ImageIO.read(new File("Source//backgroundImage.png"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            public void paint( Graphics g ) {
+                super.paint(g);
+                g.drawImage(backgroundImage, 0, 0, null);
+            }
+        };
         pictureBox = new JPanel();
         frame.pack();
         frame.setContentPane(pictureBox);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setSize(1000, 600);
         frame.setLocationRelativeTo(null);
+
         try {frame.setIconImage(new ImageIcon(ImageIO.read(new File("Source//frameIcon1.png"))).getImage());
         } catch (IOException e) {
             throw new RuntimeException(e);
